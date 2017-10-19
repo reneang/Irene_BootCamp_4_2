7d4770f18bb0e9f3c5d9b568a48bec8be5fd6900
reneang <ireneribkang@gmail.com>
2017-10-19 16:04:46 +0700

    4:04 PM

--- Frontend/src/app/api.service.ts
+++ Frontend/src/app/api.service.ts
@@ -9,9 +9,9 @@ export class APIService {
   courseduration: string  
   CourseList: object[] = 
   [
-    {'id':'1','coursename' : 'Math' , 'coursecode' : '209A' , 'courseduration' : '1 month'} , 
-    {'id':'2','coursename' : 'PHP' , 'coursecode' : '210A','courseduration' : '10 day'}, 
-    {'id':'3','coursename' : 'Angular' ,'coursecode' : '890A', 'courseduration' : '9 day'} 
+    {'id':1,'coursename' : 'Math' , 'coursecode' : '209A' , 'courseduration' : '1 month'} , 
+    {'id':2,'coursename' : 'PHP' , 'coursecode' : '210A','courseduration' : '10 day'}, 
+    {'id':3,'coursename' : 'Angular' ,'coursecode' : '890A', 'courseduration' : '9 day'} 
   ];
   
   constructor() { }
